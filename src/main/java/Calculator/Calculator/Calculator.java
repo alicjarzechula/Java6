8b030c7ecb59cf0df706e2a82cef4b7572f0ee40
“Alicja <a.rzechula3@gmail.com>
2018-01-23 18:22:21 +0100

    "Calculator"

---
+++ src/main/java/Calculator/Calculator/Calculator.java
@@ -0,0 +1,16 @@
+package Calculator.Calculator;
+
+public class Calculator {
+    public float plus(float a, float b){
+        return a+b;
+    }
+    public float minus(float a, float b){
+        return a-b;
+    }
+    public float times(float a, float b){
+        return a*b;
+    }
+    public float by(float a, float b){
+        return a/b;
+    }
+}
