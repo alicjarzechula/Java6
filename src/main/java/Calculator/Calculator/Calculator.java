a4a7e2ba0598ea062b6b4de757770f34f5e2ba69
“Alicja <a.rzechula3@gmail.com>
2018-01-24 13:16:13 +0100

    Renamed plus, minus, times, by

--- src/main/java/Calculator/Calculator/Calculator.java
+++ src/main/java/Calculator/Calculator/Calculator.java
@@ -1,16 +1,16 @@
 package Calculator.Calculator;
 
 public class Calculator {
-    public float plus(float a, float b){
+    public float add(float a, float b){
         return a+b;
     }
-    public float minus(float a, float b){
+    public float substrack(float a, float b){
         return a-b;
     }
-    public float times(float a, float b){
+    public float multiply(float a, float b){
         return a*b;
     }
-    public float by(float a, float b){
+    public float divide(float a, float b){
         if(b==0){
             throw new IllegalArgumentException("Division by 0!");
         }
